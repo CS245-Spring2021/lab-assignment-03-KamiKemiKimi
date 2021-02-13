d267d72f69dddf9eae4a1aada5ef66c1cc6cc5b1
KamiKemiKimi <alexkim280@gmail.com>
2021-02-12 18:03:49 -0800

    Finished Wiggle Sort

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Lab_Assignment_03_Template
-Template for wigglesort
+# Lab_Assignment_03 Analysis
+
+
+Debrief:
+Used insertion sort method to sort.
+Then used wiggle sort by taking the lowest and 
+highest values of the array alternatively.
+
+
+Analysis:
+The runtime of wiggle sort is O(n^2). This is because
+I put insertion sort in the same function as wiggle sort.
+And the worst case scenario is that all of the numbers were
+reversed from greatest to least.
+If insertion sort was a separate function, then wiggle sort
+would be O(1) assuming that the array of random numbers
+were sorted by insertion sort first.
+The space analysis (although not required to mention) 
+would be O(n) as there is an array declaration.
--- WiggleSort.java
+++ WiggleSort.java
@@ -2,7 +2,7 @@
  * NO EXTERNAL LIBRARIES OR PACKAGES SHOULD BE IMPORTED
  * Sorts an array into WiggleSort format
  *
- * @author 
+ * @author Alex Kim
  */
 public class WiggleSort {
     /**
@@ -11,6 +11,49 @@ public class WiggleSort {
      * @return sorted array
     */
     public static int[] wiggleSort(int[] nums) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    	//insertion sort
+    	for (int i = 1; i < nums.length; i++)
+    	{
+    		//copy
+    		int temp = nums[i];
+    		int j = i - 1;
+    		
+    		//reassign
+    		while(j >= 0 && nums[j] > temp)
+    		{
+    			nums[j+1] = nums[j];
+    			--j;
+    		}
+    		
+    		//re-copy
+    		nums[j+1] = temp;
+    	}
+    	
+    	//wiggle sort
+    	int [] arr = new int[nums.length];
+    	int low = 0;
+    	int high = arr.length - 1;
+    	
+    	//determine whether we use lower or high number with boolean
+    	boolean lower = true;
+    	
+    	for (int j = 0; j < arr.length; j++)
+    	{
+    		if (lower)
+    		{
+    			arr[j] = nums[low];
+    			low++;
+    			lower = false;
+    		}
+    		else if (!lower)
+    		{
+    			arr[j] = nums[high];
+    			high--;
+    			lower = true;
+    		}	
+    	}
+    	
+    	//return new array
+    	return arr;
     }
 }
